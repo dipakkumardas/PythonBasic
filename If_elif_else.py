2a3f2e9136f267f4d93b621eb05f23f786cea1eb
Dipak <das.dipak19@gmail.com>
2023-06-12 22:43:46 +0530

    New Changes

---
+++ If_elif_else.py
@@ -0,0 +1,9 @@
+x = int(input("Enter X :"))
+y = int(input("Enter Y :"))
+
+if x > y:
+    print("x is Grater than Y")
+elif y > x:
+    print("Y is grated than x")
+else:
+    print("X and Y Both are Equal")
