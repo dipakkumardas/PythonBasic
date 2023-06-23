4239446da143313e3bf7ea4f304f5835d47e4101
Dipak <das.dipak19@gmail.com>
2023-06-24 00:15:10 +0530

    Home Work

---
+++ HomeWork/FindMaximumSumandMultiplyNumber.py
@@ -0,0 +1,28 @@
+input_string = (input("Enter Number of a list separated by space: "))
+
+number_list = input_string.split()
+
+# print List
+print('list:',number_list)
+
+# convert each item to Interger Type
+
+for i in range(len(number_list)):
+    # convert each type o int type
+    number_list[i] = int(number_list[i])
+
+print("Maximum of Inputter no:",max(number_list))
+print("Minimum of Inputted No :",min(number_list))
+print("Sum of all List Number :",sum(number_list))
+
+multiplied = []
+for number in number_list:
+    multiplied.append(number*2)
+print("Multiply of the List :",multiplied)
+
+
+
+
+
+
+
