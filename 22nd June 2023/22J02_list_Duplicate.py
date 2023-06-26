5de15437f92216aea668cfd44dd02f0f0cb195cd
Dipak <das.dipak19@gmail.com>
2023-06-27 00:57:09 +0530

    New changes

--- 22nd June 2023/22J02_list_Duplicate.py
+++ 22nd June 2023/22J02_list_Duplicate.py
@@ -7,12 +7,16 @@ non_dup = []
 
 # Normal Logic
 # Go through the all items if item is present in the list, add to new non_dup_list
-for i in my_list:
-    if i not in non_dup:
-        non_dup.append(i)
-print(non_dup)
+# for i in my_list:
+#     if i not in non_dup:
+#         non_dup.append(i)
+# print(non_dup)
 
+# One Line Satement for for loop see below :
+[non_dup.append(i) for i in my_list if i not in non_dup]
+print(non_dup)
 #Set Function
-print("This is the List with duplicate value :", my_list)
-non_dup2 = list(set(my_list))
-print("This is the list  after removing duplicate value :",non_dup2)
+# Set  - list of unordered and unique items
+# print("This is the List with duplicate value :", my_list)
+# non_dup2 = list(set(my_list))
+# print("This is the list  after removing duplicate value :",non_dup2)
---
+++ 22nd June 2023/22J05_TakeInputAndStoreInList.py
@@ -0,0 +1,7 @@
+list = []
+n = int(input("Enter the Number of Element that you want to Enter :"))
+
+for i in range(0,n):
+    temp = int(input())
+    list.append(temp)
+print(list)
