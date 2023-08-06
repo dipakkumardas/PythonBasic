71018aabf418f3ec746d7885ae6ea3b290eaea11
Dipak <das.dipak19@gmail.com>
2023-08-06 21:40:21 +0530

    Selenium3

---
+++ 1stAug2023/test_selenium03.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+import logging
+
+
+def test_login():
+    # Chrome --> Chrome Option
+    chrome_options = webdriver.ChromeOptions()
+    # extension_path = "G:/CRX/adblocker1.crx"
+    chrome_options.add_argument("--start-maximized")
+    #chrome_options.add_argument('--headless=new')
+    # chrome_options.add_extension(extension_path)
+    driver = webdriver.Chrome(chrome_options)
+    # driver.maximize_window()
+    driver.get("https://app.vwo.com")
