9b0e5e57e183611542ec75413f7f674071715648
Dipak <das.dipak19@gmail.com>
2023-08-21 00:11:07 +0530

    Find specific Data

--- 16thAug2023/test_01_webtable01.py
+++ 16thAug2023/test_01_webtable01.py
@@ -9,10 +9,28 @@ def test_webtables():
     driver.maximize_window()
     driver.get("https://awesomeqa.com/webtable.html")
     time.sleep(2)
-    row_element = driver.find_elements(By.XPATH, "//table[@id='customers']//tbody//tr")
-    row = len(row_element)
+    row_elements = driver.find_elements(By.XPATH, "//table[@id='customers']//tbody//tr")
+    row = len(row_elements)
     print("The No Of Row is :", row)
-    col_element = driver.find_elements(By.XPATH, "//table[@id='customers']//tbody//tr//th")
-    col = len(col_element)
+    col_elements = driver.find_elements(By.XPATH, "//table[@id='customers']//tbody//tr//th")
+    col = len(col_elements)
     print("The No Of Column is :", col)
-    driver.quit()
+
+    first_part = "//table[@id='customers']//tbody/tr["
+    second_part = "]/td["
+    third_part = "]"
+
+    for i in range(2, row + 1):
+        for j in range(1, col + 1):
+            dynamic_path = f"{first_part}{i}{second_part}{j}{third_part}"
+            data = driver.find_element(By.XPATH, dynamic_path)
+            # print(dynamic_path)
+            print(data.text, end=" ")
+
+            if "Helen Bennett" in data:
+                country_path = f"{dynamic_path}/following-sibling::td"
+                print(country_path)
+                country_text = driver.find_element(By.XPATH, country_path).text
+                print(f"Helen Bennet is in {country_text}")
+
+
